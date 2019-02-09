f0d1ae3a837dc88e8a3cac995cf0ddc93d7cfc74
ElSenor64 <sillynoodle36@gmail.com>
2019-02-09 15:31:00 -0700

    Working on Ultrasonic Lift Positioning.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -56,9 +56,11 @@ public class Constants {
 	static final int MID_RIGHT_MOTOR_ID = 3;
 	static final int BACK_RIGHT_MOTOR_ID = 7;
 
-	static final double LIFT_SPEED = 1; //Make -1 to reverse direction
+	static final double LIFT_SPEED = 1; //Make -1 to reverse direction, use values 1, -1, and in between. (Probably not 0.)
 	static final int ULTRASONIC_HATCH_LEFT_PORT = 0; //SET PORTS!!!
 	static final int ULTRASONIC_HATCH_RIGHT_PORT = 1; //SET PORTS!!!
+	static final double DISTANCE_AT_LIFT = 12; //Inches
+	static final double ULTRASONIC_THRESHOLD = 11; //Inches
 
 	static final double DRIVE_SPEED = 0.95;
 	static final double DRIVE_TURN_SPEED = 0.75;
--- src/main/java/frc/robot/Lift.java
+++ src/main/java/frc/robot/Lift.java
@@ -9,7 +9,7 @@ public class Lift {
     public enum LiftStates {
         LOCK, STOP, EN, EC
     }
-
+    
     LiftStates liftStates;
     private WPI_TalonSRX frontLiftMotor;
     private WPI_TalonSRX backLiftMotor;
@@ -18,7 +18,11 @@ public class Lift {
     private Ultrasonic ultraLeft;
     private Ultrasonic ultraRight;
 
-
+    //private boolean aligned;
+    boolean alignedDistance;
+    boolean alignedAngle;
+    private double distanceAtLift;
+    
     public Lift(){
         frontLiftMotor = new WPI_TalonSRX(Constants.FRONT_LIFT_MOTOR_ID);
         backLiftMotor = new WPI_TalonSRX(Constants.BACK_LIFT_MOTOR_ID);
@@ -26,6 +30,11 @@ public class Lift {
 
         ultraLeft = new Ultrasonic(Constants.ULTRASONIC_HATCH_LEFT_PORT);
         ultraRight = new Ultrasonic(Constants.ULTRASONIC_HATCH_RIGHT_PORT);
+
+        //aligned = false;
+        alignedDistance = false;
+        alignedAngle = false;
+        distanceAtLift = Constants.DISTANCE_AT_LIFT;
     }
 
     public void initialize() {
@@ -41,19 +50,78 @@ public class Lift {
 
     public double distanceRatio(Ultrasonic left, Ultrasonic right){
         double ratio;
-        //This method converts the distance between the two ultrasonic sensors into a compact ratio.
+        //This method converts the distance between the two ultrasonic sensors into a compact ratio. Ratio will never be less than ZERO or greater than TWO.
         // Left:  33inches
         // -----  --------  =  0.942857...              Also: ratio < 1 means a greater RIGHT distance
         // Right: 35inches      ^ Returned value              ratio > 1 means a greater LEFT distance
 
         ratio = (left.getImperialUltrasonicValue() / right.getImperialUltrasonicValue());
+        if(ratio >= 2){         //If the raw ratio is 2 or greater (Left distance is 2 or greater times larger than Right distance), set it to max adjust right.
+            ratio = 1.99999999;
+        }
+
         return ratio;
     }
 
-    public void distanceAlign(){
-        double ratio = distanceRatio(ultraLeft, ultraRight);
+    public boolean distanceAlign(){
+        double ultraLeftDistance;
+        double ultraRightDistance;
+        double leftTankSpeed;
+        double rightTankSpeed;        
+
+      //Align Angle
+        if(alignedAngle == false){
+            double ratio = distanceRatio(ultraLeft, ultraRight);
+
+            if (ratio > 1){ //Left Father
+                leftTankSpeed = (2-ratio); //Figure out a way to return these
+                rightTankSpeed = (-leftTankSpeed); 
+
+                alignedAngle = false;
+
+            } else if (ratio < 1){  //Right Farther
+                rightTankSpeed = (1-ratio);
+                leftTankSpeed = (-rightTankSpeed);
+
+                alignedAngle = false;
+            } else {
+                leftTankSpeed = 0;
+                rightTankSpeed = 0;
+
+                alignedAngle = true;
+            }
+        }
+
+      //Align Distance
+        else if(alignedDistance == false){
+            ultraLeftDistance = ultraLeft.getImperialUltrasonicValue();
+            ultraRightDistance = ultraRight.getImperialUltrasonicValue();
+
+            if(ultraLeftDistance > distanceAtLift || ultraRightDistance > distanceAtLift){ //Farther than target
+                //Move forward (speed variable based on distance)
 
 
+            } else if (ultraLeftDistance == Constants.ULTRASONIC_THRESHOLD){ //At threshold
+                //Back up at moderate pace
+
+            } else if (ultraLeftDistance < distanceAtLift || ultraRightDistance < distanceAtLift) { //Closer than target
+                //Move Backard (speed variable based on distance)
+
+            } else {  //At target
+                //Stop Motos, set alignDistance true, and initialize another angle check.
+
+                alignedDistance = true;
+                alignedAngle = false;
+            }
+        }
+
+      //Return
+        if(alignedDistance == true && alignedAngle == true){  //Aligned in both manners?
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 
     public void lift(Toggle liftIsDeployed){
