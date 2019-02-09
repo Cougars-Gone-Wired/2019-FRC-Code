1a29d974782300139da14813bf079c25d3c49ed7
MTGruul <jacobjosephhartt@gmail.com>
2019-02-08 19:30:01 -0700

    WACKKKKK

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,35 +4,38 @@ public class Constants {
 
 	//Manipulator
 	static final int MANIPULATOR_CONTROLLER_PORT = 1;
+	static final int MID_SWITCH_PORT = 0;
+
+	static final int CARGO_ARM_AXIS = 1; //
     static final int HATCH_ARM_AXIS = 1;
     static final int HATCH_ARM_SCHEME_BUTTON = 6;
-    static final int HATCH_ARM_GRAB_BUTTON = 3;
-    static final int MID_SWITCH_PORT = 0;
+	static final int HATCH_ARM_GRAB_BUTTON = 3;
+	static final int CARGO_ARM_INTAKE_BUTTON = 5; //
+    static final int CARGO_ARM_OUTTAKE_BUTTON = 6; //
+    static final int CARGO_ARM_TOP_BUTTON = 4; //
+    static final int CARGO_ARM_BOTTOM_BUTTON = 1; //
+    static final int CARGO_ARM_CARGO_SHIP_BUTTON = 3; //
+    static final int CARGO_ARM_ROCKET_BUTTON = 2; //
 
     static final int HATCH_ARM_MOVE_MOTOR_ID = 8;
-    static final int HATCH_ARM_GRAB_MOTOR_ID = 9;
+	static final int HATCH_ARM_GRAB_MOTOR_ID = 9;
+	static final int CARGO_ARM_MOTOR_ID = 10; //
+    static final int CARGO_ARM_INTAKE_MOTOR_ID = 11; //
 
     static final double HATCH_ARM_MOVE_SPEED = 0.05;
     static final double HATCH_ARM_GRAB_SPEED = 0.2;
 
 	static final double HATCH_ARM_MOVE_AXIS_THRESHHOLD = 0.8;
 
-	static final int CARGO_ARM_AXIS = 1;
-    static final int CARGO_ARM_INTAKE_BUTTON = 5;
-    static final int CARGO_ARM_OUTTAKE_BUTTON = 6;
-    static final int CARGO_ARM_TOP_BUTTON = 4;
-    static final int CARGO_ARM_BOTTOM_BUTTON = 1;
-    static final int CARGO_ARM_CARGO_SHIP_BUTTON = 3;
-    static final int CARGO_ARM_ROCKET_BUTTON = 2;
-    static final int CARGO_ARM_MOTOR_ID = 10;
-    static final int CARGO_ARM_INTAKE_MOTOR_ID = 11;
-    //static final int CARGO_SHIP_LIMIT_SWITCH_ID = 0;
-    //static final int ROCKET_LIMIT_SWITCH_ID = 1;
-    static final int CARGO_ARM_ENCODER_PORT_ONE = 0;
-    static final int CARGO_ARM_ENCODER_PORT_TWO = 1;
-    static final int CARGO_SHIP_GOAL_ENCODER_VALUE = 40;
-    static final int ROCKET_GOAL_ENCODER_VALUE = 50;
-    static final int CARGO_ARM_MOTOR_SPEED = 1;
+   
+    
+    //static final int CARGO_SHIP_LIMIT_SWITCH_ID = 0; //
+    //static final int ROCKET_LIMIT_SWITCH_ID = 1; //
+    static final int CARGO_ARM_ENCODER_PORT_ONE = 0; //
+    static final int CARGO_ARM_ENCODER_PORT_TWO = 1; //
+    static final int CARGO_SHIP_GOAL_ENCODER_VALUE = 40; //
+    static final int ROCKET_GOAL_ENCODER_VALUE = 50; //
+    static final int CARGO_ARM_MOTOR_SPEED = 1; //
 
 
 	//Mobility
