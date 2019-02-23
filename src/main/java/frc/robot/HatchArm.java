d7f4795e5a03950d73a79aa9f107c8d136399e76
MTGruul <jacobjosephhartt@gmail.com>
2019-02-23 12:49:27 -0700

    replacing lots of code

--- src/main/java/frc/robot/HatchArm.java
+++ src/main/java/frc/robot/HatchArm.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 public class HatchArm {
-    // Enums
     private enum HatchArmManualMoveStates {
         NOT_MOVING, MOVING_TOWARDS_FLOOR, MOVING_TOWARDS_INITIAL
     }
@@ -19,15 +18,10 @@ public class HatchArm {
     private enum HatchArmGrabStates {
         IN, OUT, TO_OUT, TO_IN, TO_OUT_INTER, TO_IN_INTER
     }
-    public enum Grab2States {
-        NOT_MOVING, GOING_IN, GOING_OUT
-    }
-
-    // Private Instance Variables
+    
     HatchArmManualMoveStates hatchArmManualMoveState;
     HatchArmMoveStates hatchArmMoveState;
     HatchArmGrabStates hatchArmGrabState;
-    Grab2States currentGrab2State;
     private WPI_TalonSRX hatchArmMoveMotor;
     private WPI_TalonSRX hatchArmGrabMotor;
     private SensorCollection moveLimitSwitches;
@@ -35,7 +29,6 @@ public class HatchArm {
     private DigitalInput moveMidSwitch;
     private boolean moveMidSwitchValue;
 
-    // Constructor
     public HatchArm() {
         hatchArmMoveMotor = new WPI_TalonSRX(Constants.HATCH_ARM_MOVE_MOTOR_ID);
         hatchArmGrabMotor = new WPI_TalonSRX(Constants.HATCH_ARM_GRAB_MOTOR_ID);
@@ -50,15 +43,12 @@ public class HatchArm {
         initialize();
     }
 
-    // Methods
-    // I don't really need to explain this
     public void initialize() {
         hatchArmMoveMotor.set(0);
         hatchArmManualMoveState = HatchArmManualMoveStates.NOT_MOVING;
         hatchArmMoveState = HatchArmMoveStates.INSIDE;
         hatchArmGrabMotor.set(0);
         hatchArmGrabState = HatchArmGrabStates.OUT;
-        currentGrab2State = Grab2States.NOT_MOVING;
     }
 
     public void hatchArmGrab(boolean hatchArmGrabToggle) {
@@ -107,6 +97,11 @@ public class HatchArm {
         }
     }
 
+    public enum Grab2States {
+        NOT_MOVING, GOING_IN, GOING_OUT
+    }
+    Grab2States currentGrab2State = Grab2States.NOT_MOVING;
+
     public void grab2(boolean inButton, boolean outButton) {
         switch(currentGrab2State) {
             case NOT_MOVING:
@@ -133,7 +128,6 @@ public class HatchArm {
         }
     }
 
-    // This is for testing and resetting the hatch arm
     public void hatchArmManualMove(boolean lowerHatchArmButton, boolean raiseHatchArmButton) {
         switch(hatchArmManualMoveState) {
             case NOT_MOVING:
@@ -166,7 +160,6 @@ public class HatchArm {
         }
     }
 
-    // This is the main move function.  It requires that the hatch arm starts in between
     public void hatchArmMove(boolean lowerHatchArmButton, boolean raiseHatchArmButton) {
         moveMidSwitchValue = !moveMidSwitch.get();
         SmartDashboard.putString("State", hatchArmMoveState.toString());
@@ -209,6 +202,9 @@ public class HatchArm {
                 } else if (raiseHatchArmButton) {
                     hatchArmMoveMotor.set(-Constants.HATCH_ARM_MOVE_SPEED);
                     hatchArmMoveState = HatchArmMoveStates.VERT_TO_INSIDE;
+                } else if (moveLimitSwitches.isFwdLimitSwitchClosed()) {
+                    hatchArmMoveMotor.set(0);
+                    hatchArmMoveState = HatchArmMoveStates.FLOOR;
                 }
                 break;
             case VERT_TO_FLOOR:
