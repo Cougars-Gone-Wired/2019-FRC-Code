1d7b4bfc3283717ee4f1910d8a8bb939e3901dfa
moarrows <rygars5@gmail.com>
2019-03-02 15:11:50 -0700

    Added Cargo and Hatch to Dashboard

--- src/main/java/frc/robot/Dashboard.java
+++ src/main/java/frc/robot/Dashboard.java
@@ -3,8 +3,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard {
-    private Drive drive;
     private Controllers controllers;
+    private Drive drive;
     private HatchArm hatchArm;
     private CargoManip cargoManip;
     private Lift lift;
@@ -12,20 +12,23 @@ public class Dashboard {
     public int refreshCount = 0;
 
     public Dashboard(Robot robot) {
-        drive = robot.getDrive();
         controllers = robot.getControllers();
+        drive = robot.getDrive();
         hatchArm = robot.getHatchArm();
         cargoManip = robot.getCargoManip();
         lift = robot.getLift();
     }
 
-    public void showDisabledValues() {
-        refreshDashboard();
-        
-    }
-
+    
     public void showEnabledValues() {
         drive.showDashboard();
+        hatchArm.displayValues();
+        cargoManip.sensorLight();
+    }
+
+    public void showDisabledValues() {
+        refreshDashboard();
+
     }
 
     public void refreshDashboard() {
