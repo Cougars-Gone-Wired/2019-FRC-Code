e0a096ab43c331f0987aae7678ee04b3022b8afa
Ethan Shahan <tieguy10501@gmail.com>
2019-03-15 18:18:25 -0600

    Adds Limelight

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,26 +1,10 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
-
 public class Robot extends TimedRobot {
-  //private Cameras cameras;
-  private Cameras2 cameras2;
+  private Cameras cameras;
 
 	private Controllers controllers;
   private CargoManip cargoManip;
@@ -30,16 +14,8 @@ public class Robot extends TimedRobot {
   //Lift code
   private Lift lift;
 
-  //private Ultrasonic leftHatchUltrasonic;
-  //private Ultrasonic rightHatchUltrasonic;
-
   private Dashboard dashboard;
   private Logging logging;
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
 
   @Override
   public void robotInit() {
@@ -52,25 +28,13 @@ public class Robot extends TimedRobot {
 
     lift = new Lift();
 
-    //leftHatchUltrasonic = new Ultrasonic(Constants.LEFT_HATCH_ULTRASONIC_SENSOR_PORT);
-    //rightHatchUltrasonic = new Ultrasonic(Constants.RIGHT_HATCH_ULTRASONIC_SENSOR_PORT);
-
     dashboard = new Dashboard(this);
     logging = new Logging(this);  
     logging.activeInitialize();
 
-    cameras2 = new Cameras2();
-    //new Thread(cameras2).start();;
+    cameras = new Cameras();
+    new Thread(cameras).start();;
   }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
 
   @Override
   public void robotPeriodic() {
@@ -78,22 +42,10 @@ public class Robot extends TimedRobot {
     dashboard.showDisabledValues();
 
     if (controllers.getCameraStopButton().toggle()) {
-      cameras2.stop();
+      cameras.stop();
       controllers.getCameraStopButton().setOutput(false);
     }
   }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
 
   @Override
   public void autonomousInit() {
@@ -103,21 +55,12 @@ public class Robot extends TimedRobot {
     cargoManip.initialize();
     drive.initalize();
     hatchArm.initialize();
-
-    //leftHatchUltrasonic.initialize();
-    //rightHatchUltrasonic.initialize();
-    
+ 
     logging.activeInitialize();
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-
   @Override
   public void autonomousPeriodic() {
-    //cameras.cameraVideo();
-
     controllers.setControllerValues();
     setSide(controllers.getDriveToggleValue());
 
@@ -127,28 +70,18 @@ public class Robot extends TimedRobot {
     //cargoManip.sensorLight();
 
     //drive.setSide(controllers.getDriveToggleValue());
-    //drive.setMode(controllers.getUltrasonicToggleValue());
     drive.setFine(controllers.getDriveFineToggleValue());
+    drive.setTrack(controllers.isTrackButton());
     drive.robotDrive(controllers.getDriveSpeedAxis(), controllers.getDriveTurnAxis());
-    //drive.showDashboard();
 
     hatchArm.hatchArmManualMove(controllers.getHatchArmAxis());
     // hatchArm.hatchArmMove(controllers.getLowerHatchArmButton(), controllers.getRaiseHatchArmButton());
     
-    //leftHatchUltrasonic.setUltrasonicValues();
-    //rightHatchUltrasonic.setUltrasonicValues();
-    //leftHatchUltrasonic.displayValues("Left Ultrasonic");
-    //rightHatchUltrasonic.displayValues("Right Ultrasonic");
-
     dashboard.showEnabledValues();
 
     //The Loggers chopping down trees
     logging.collectData();
   }
-
-  /**
-   * This function is called when teleop is initialzied.
-   */
 
   @Override
   public void teleopInit() {
@@ -160,19 +93,11 @@ public class Robot extends TimedRobot {
 
     lift.initialize();
 
-    //leftHatchUltrasonic.initialize();
-    //rightHatchUltrasonic.initialize();
-
     logging.activeInitialize();
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-
   @Override
   public void teleopPeriodic() {
-    //cameras.cameraVideo();
 
     controllers.setControllerValues();
     setSide(controllers.getDriveToggleValue());
@@ -182,10 +107,9 @@ public class Robot extends TimedRobot {
     cargoManip.intakeMove(controllers.getCargoArmIntakeAxis(), controllers.getCargoArmOuttakeAxis());
     //cargoManip.sensorLight();
 
-    //drive.setMode(controllers.getUltrasonicToggleValue());
     drive.setFine(controllers.getDriveFineToggleValue());
+    drive.setTrack(controllers.isTrackButton());
     drive.robotDrive(controllers.getDriveSpeedAxis(), controllers.getDriveTurnAxis());
-    //drive.showDashboard();
 
     hatchArm.hatchArmManualMove(controllers.getHatchArmAxis());
     // hatchArm.hatchArmMove(controllers.getLowerHatchArmButton(), controllers.getRaiseHatchArmButton());
@@ -195,20 +119,11 @@ public class Robot extends TimedRobot {
     // lift.lift(controllers.isLiftDeployButton(), controllers.isLiftRetractButton(), controllers.isLiftStopButton(), controllers.isLiftWithdrawFromStairButton(), drive, controllers.getMobilityStick());
     lift.lift2(controllers.isLiftDeployButton(), controllers.isLiftRetractButton());
     
-    //leftHatchUltrasonic.setUltrasonicValues();
-    //rightHatchUltrasonic.setUltrasonicValues();
-    //leftHatchUltrasonic.displayValues("Left Ultrasonic");
-    //rightHatchUltrasonic.displayValues("Right Ultrasonic");
-
     dashboard.showEnabledValues();
 
     //The Loggers chopping down trees
     logging.collectData();
   }
-
-  /**
-   * This function is called periodically during test mode.
-   */
 
   @Override
   public void testPeriodic() {
@@ -238,7 +153,6 @@ public class Robot extends TimedRobot {
 
   public void setSide(boolean sideToggle) {
     drive.setSide(sideToggle);
-    //cameras.setSide(sideToggle);
     Sides.setSide(sideToggle);
   }
 
@@ -265,21 +179,4 @@ public class Robot extends TimedRobot {
   public Logging getLogging() {
     return logging;
   }
-
-  /*public Ultrasonic getLeftHatchUltrasonic() {
-    return leftHatchUltrasonic;
-  }*/
-
-  /*public Ultrasonic getRightHatchUltrasonic() {
-    return rightHatchUltrasonic;
-  }*/
-
-  /*public double getLeftHatchUltrasonicImperialValue() {
-    return leftHatchUltrasonic.getImperialUltrasonicValue();
-  }*/
-
-  /*public double getRightHatchUltrasonicImperialValue() {
-    return rightHatchUltrasonic.getImperialUltrasonicValue();
-  }*/
-
 }
